b6097ee6171b8d08829f2bda619dc21ab0a03ea4
thomaswyee <thomas.w.yee@gmail.com>
2021-01-13 18:03:41 +0000

    initials

--- client.js
+++ client.js
@@ -5,16 +5,16 @@ const net = require('net');
  */
 const connect = function() {
   const conn = net.createConnection({
-    host: "10.0.2.15",
+    host: "localhost",
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log('data has come!');
-    console.log(data);
+  conn.on('connect', () => {
+    console.log('"Successfully connected to game server"');
+    conn.write('Name: WII');
   });
+  return conn;
 };
 
 module.exports = {connect};
